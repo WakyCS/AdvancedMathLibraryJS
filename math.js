906f3580c2b2a01d425d5bdfd0a1e785d5f164a3
WakyCS <rfttgbbf@outlook.com>
2025-03-15 19:06:06 -0400

    Create math.js

---
+++ math.js
@@ -0,0 +1,12 @@
+function findNextInQuadratic(n, sequence) {
+    let dif1_1 = sequence[1]-sequence[0];
+    let dif1_2 = sequence[2]-sequence[1];
+    
+    let dif2_1 = dif1_2-dif1_1;
+    
+    let a = dif2_1/2;
+    let b = dif1_1-(3*a);
+    let c = sequence[0]-b-a;
+    
+    return a*Math.pow(n, 2)+b*n+c
+}
