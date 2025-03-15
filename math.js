09432b6a40c9afa7e52651f546e46c20f2b51a79
WakyCS <rfttgbbf@outlook.com>
2025-03-15 19:26:15 -0400

    Update math.js

--- math.js
+++ math.js
@@ -1,20 +1,31 @@
 (function (global) {
-    const functions = {
-        findNextInQuadratic: function (n, sequence) {
-            let dif1_1 = sequence[1] - sequence[0];
-            let dif1_2 = sequence[2] - sequence[1];
+  const functions = {
+    findNthInSequence: function (n, sequence) {
+      if (sequence.length < 4) {
+        if (sequence[1] - sequence[0] == sequence[2] - sequence[1]) {
+          let d = sequence[1] - sequence[0];
+          let a1 = sequence[0];
 
-            let dif2_1 = dif1_2 - dif1_1;
+          return a1 + (n - 1) * d;
+        } else {
+          let dif1_1 = sequence[1] - sequence[0];
+          let dif1_2 = sequence[2] - sequence[1];
 
-            let a = dif2_1 / 2;
-            let b = dif1_1 - (3 * a);
-            let c = sequence[0] - b - a;
+          let dif2_1 = dif1_2 - dif1_1;
 
-            return a * Math.pow(n, 2) + b * n + c;
-        },
-    };
+          let a = dif2_1 / 2;
+          let b = dif1_1 - 3 * a;
+          let c = sequence[0] - b - a;
 
-    for (let key in functions) {
-        global[key] = functions[key];
-    }
+          return a * Math.pow(n, 2) + b * n + c;
+        }
+      } else {
+        return null;
+      }
+    },
+  };
+
+  for (let key in functions) {
+    global[key] = functions[key];
+  }
 })(window);
