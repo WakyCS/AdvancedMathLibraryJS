fc060d6787287240960cc86f3e88d3996c150ff4
WakyCS <rfttgbbf@outlook.com>
2025-03-15 19:11:05 -0400

    Update math.js

--- math.js
+++ math.js
@@ -1,33 +1,17 @@
 (function (global) {
-    const MyLibrary = {
-        functions: {},
+    function findNextInQuadratic(n, sequence) {
+        let dif1_1 = sequence[1] - sequence[0];
+        let dif1_2 = sequence[2] - sequence[1];
 
-        addFunction: function(name, fn) {
-            this.functions[name] = fn;
-        },
+        let dif2_1 = dif1_2 - dif1_1;
 
-        callFunction: function(name, ...args) {
-            if (this.functions[name]) {
-                return this.functions[name](...args);
-            } else {
-                console.warn(`Function '${name}' not found.`);
-            }
-        },
+        let a = dif2_1 / 2;
+        let b = dif1_1 - (3 * a);
+        let c = sequence[0] - b - a;
 
-        findNextInQuadratic: function(n, sequence) {
-            let dif1_1 = sequence[1] - sequence[0];
-            let dif1_2 = sequence[2] - sequence[1];
+        return a * Math.pow(n, 2) + b * n + c;
+    }
 
-            let dif2_1 = dif1_2 - dif1_1;
-
-            let a = dif2_1 / 2;
-            let b = dif1_1 - (3 * a);
-            let c = sequence[0] - b - a;
-
-            return a * Math.pow(n, 2) + b * n + c;
-        }
-    };
-
-    // Expose the library globally
-    global.MyLibrary = MyLibrary;
+    // Attach functions directly to the global window object
+    global.findNextInQuadratic = findNextInQuadratic;
 })(window);
