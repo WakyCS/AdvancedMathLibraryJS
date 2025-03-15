57a4af8dfd28d8afe97fa25bde111ead6eb75365
WakyCS <rfttgbbf@outlook.com>
2025-03-15 19:14:59 -0400

    Update math.js

--- math.js
+++ math.js
@@ -1,17 +1,20 @@
 (function (global) {
-    function findNextInQuadratic(n, sequence) {
-        let dif1_1 = sequence[1] - sequence[0];
-        let dif1_2 = sequence[2] - sequence[1];
+    const functions = {
+        findNextInQuadratic: function (n, sequence) {
+            let dif1_1 = sequence[1] - sequence[0];
+            let dif1_2 = sequence[2] - sequence[1];
 
-        let dif2_1 = dif1_2 - dif1_1;
+            let dif2_1 = dif1_2 - dif1_1;
 
-        let a = dif2_1 / 2;
-        let b = dif1_1 - (3 * a);
-        let c = sequence[0] - b - a;
+            let a = dif2_1 / 2;
+            let b = dif1_1 - (3 * a);
+            let c = sequence[0] - b - a;
 
-        return a * Math.pow(n, 2) + b * n + c;
+            return a * Math.pow(n, 2) + b * n + c;
+        },
+    };
+
+    for (let key in functions) {
+        global[key] = functions[key];
     }
-
-    // Attach functions directly to the global window object
-    global.findNextInQuadratic = findNextInQuadratic;
 })(window);
